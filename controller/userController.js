5de69575b8ee86ae6d8cb36dc342b33c5969fe75
msamil93 <103052903+msamil93@users.noreply.github.com>
2022-12-15 07:27:49 +0300

    Added new functions to userController
    Author: Şamil Tekinoğlu
    Date: 15.12.2022
    
       Previously users do not have an
       option to follow others.
    
       So to methods (follow and unfollow)
       had been added to the
       userController
    
       As a result now users can follow or
       unfollow other users

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,191 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import News from "../models/newsModel.js";
 
 const createUser = async (req,res) => {
     
     try {
         const user = await User.create(req.body)
-        res.status(201).json({
-            succeded: true,
-            user,
+        res.status(201).json({user: user._id})
+        
+    } catch (error) {
+
+        let errors = {}
+
+        if(error.code === 11000) {
+            errors.email = "This email is already registered"
+        }
+
+        if(error.name === "ValidationError") {
+            Object.keys(error.errors).forEach((key) => {
+                errors[key] = error.errors[key].message;
+            });
+        }
+
+
+        res.status(400).json(errors);
+    }
+};
+
+const loginUser = async (req,res) => {
+    
+    try {
+        const {username, password} = req.body;
+
+  
+
+        const user = await User.findOne({username})
+
+        let same = false;
+
+        if(user) {
+            same = await bcrypt.compare(password, user.password)
+        }
+        else {
+            return res.status(401).json ({
+                succeded: false,
+                error: "There is no such user!"
+            });
+        }
+
+        if(same) {
+
+            const token = createToken(user._id)
+            res.cookie("jwt", token, {
+                httpOnly: true,
+                maxAge: 1000*60*60*24,
+            })
+
+            res.redirect("/user/dashboard");
+            
+        } else {
+            res.status(401).json ({
+                succeded: false,
+                error: "Wrong password",
+            });
+        }
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
         });
+    }
+};
+
+const createToken = (userId) => {
+    return jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+    }); 
+};
+
+const getDashboardPage = async (req,res) => {
+    const news= await News.find({user: res.locals.user._id});
+    const user= await User.findById({_id: res.locals.user._id}).populate([
+        "followings", 
+        "followers",
+    ])
+    res.render("dashboard", {
+        link: "dashboard",
+        news,
+        user,
+    })
+}
+
+const getAllUsers = async (req,res) => {
+    
+    try {
+        const users = await User.find({ _id: {$ne: res.locals.user._id}})
+        res.status(200).render("users",  {
+            users,
+            link: "users",
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+const getAUser = async (req,res) => {
+    
+    try {
+        const user = await User.findById ({_id: req.params.id});
+
+        const inFollowers = user.followers.some((follower)=> {
+            return follower.equals(res.locals.user._id) 
+        });
+
+        const news = await News.find ({user: user._id});
+        res.status(200).render("user",  {
+            user,
+            news,
+            link: "user",
+            inFollowers,
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+const follow = async (req,res) => {
+    
+    try {
+
+        let user = await User.findByIdAndUpdate(
+            {_id: req.params.id},
+            {
+                $push: {followers: res.locals.user._id}
+            },
+            {new: true},
+            )
+
+            user = await User.findByIdAndUpdate(
+                {_id: res.locals.user._id},
+                {
+                    $push: {followings: req.params.id},
+                },
+                {new: true},
+            );
+
+            res.status(200).redirect(`/user/${req.params.id}`)
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+const unfollow = async (req,res) => {
+    
+    try {
+
+        let user = await User.findByIdAndUpdate(
+            {_id: req.params.id},
+            {
+                $pull: {followers: res.locals.user._id}
+            },
+            {new: true},
+            )
+
+            user = await User.findByIdAndUpdate(
+                {_id: res.locals.user._id},
+                {
+                    $pull: {followings: req.params.id},
+                },
+                {new: true},
+            );
+
+            res.status(200).redirect(`/user/${req.params.id}`)
         
     } catch (error) {
         res.status(500).json({
@@ -18,4 +196,7 @@ const createUser = async (req,res) => {
 };
 
 
-export {createUser};
+
+
+
+export {createUser, loginUser, getDashboardPage, getAllUsers, getAUser, follow, unfollow};
