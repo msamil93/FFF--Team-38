4350270ac37f939acae40fde2c28be8a88c05f8f
msamil93 <103052903+msamil93@users.noreply.github.com>
2022-12-15 07:37:00 +0300

    New functions added to newsController
    Author: Samil Tekinoglu
    Date: 15.12.2022
    
        Previously there was no delete and
        update options for users to change
        news.
    
       With this change delete and update
       functions added  to newsController.
    
       Now it is possible for users to update
       or delete the news that they created

--- controller/newsController.js
+++ controller/newsController.js
@@ -1,13 +1,29 @@
 import News from "../models/newsModel.js";
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
 
 const createNews = async (req,res) => {
+
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename: true,
+            folder: "team_38",
+        }
+    );
     
+
     try {
-        const news = await News.create(req.body)
-        res.status(201).json({
-            succeded: true,
-            news,
+        await News.create({
+            name: req.body.name,
+            description: req.body.description,
+            user: res.locals.user._id,
+            url: result.secure_url,
+            image_id: result.public_id,
         });
+
+        fs.unlinkSync(req.files.image.tempFilePath);
+        res.status(201).redirect("/user/dashboard");
         
     } catch (error) {
         res.status(500).json({
@@ -37,10 +53,19 @@ const getAllNews = async (req,res) => {
 const getANews = async (req,res) => {
     
     try {
-        const a_news = await News.findById ({_id: req.params.id})
+        const a_news = await News.findById ({_id: req.params.id}).populate("user");
+
+
+        let isOwner = false
+
+        if(res.locals.user) {
+            isOwner= a_news.user.equals(res.locals.user._id)
+        }
+
         res.status(200).render("a_news",  {
             a_news,
             link: "news",
+            isOwner,
         })
         
     } catch (error) {
@@ -51,4 +76,64 @@ const getANews = async (req,res) => {
     }
 };
 
-export {createNews, getAllNews, getANews};
+const deleteNews = async (req,res) => {
+    
+    try {
+        
+        const news= await News.findById(req.params.id)
+        const newsId= news.image_id
+
+        await cloudinary.uploader.destroy(newsId)
+        await News.findOneAndRemove({_id: req.params.id})
+
+        res.status(200).redirect("/user/dashboard");
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+const updateNews = async (req,res) => {
+    
+    try {
+
+        const news= await News.findById(req.params.id)
+
+        if(req.files) {
+            const newsId= news.image_id
+            await cloudinary.uploader.destroy(newsId);
+
+            const result = await cloudinary.uploader.upload(
+                req.files.image.tempFilePath,
+                {
+                    use_filename: true,
+                    folder: "team_38",
+                }
+            );
+            
+            news.url= result.secure_url
+            news.image_id= result.public_id
+
+            fs.unlinkSync(req.files.image.tempFilePath);
+
+        }
+
+        news.name= req.body.name;
+        news.description = req.body.description;
+
+        news.save();
+
+        res.status(200).redirect(`/news/${req.params.id}`)
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+export {createNews, getAllNews, getANews, deleteNews, updateNews};
