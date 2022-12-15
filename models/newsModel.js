fc664dddfde3707a7a04ae67d0cd7c28608904a9
msamil93 <103052903+msamil93@users.noreply.github.com>
2022-12-15 07:58:46 +0300

    New attributes were added to the schemas in models
    Author: Şamil tekinoğlu
    Date: 15.12.2022
    
         Previously in the models there were
         not some attributes like follower or
         followings which users have to
         have.
    
         Now neccessary attributes are
         added to users' and news' model,
         so that the app provides important
         feautures such as follow.
         chracteristic

--- models/newsModel.js
+++ models/newsModel.js
@@ -17,6 +17,18 @@ const newsSchema = new Schema ({
         type: Date,
         default: Date.now,
     },
+    user: {
+        type:Schema.Types.ObjectId,
+        ref: 'User', 
+    },
+    url: {
+        type: String,
+        required: true
+    },
+
+    image_id: {
+        type: String,
+    },
 });
 
 const News = mongoose.model("News", newsSchema)
