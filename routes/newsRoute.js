3a3485899c44259cbd4a1a5cf3497080ef3a68f4
msamil93 <103052903+msamil93@users.noreply.github.com>
2022-12-15 08:40:39 +0300

    Send Mail Function added
    Author: Şamil Tekinoğlu
    Date: 15.12.2022
    
         Send mail function added to page
         controller
    
         So that users can report any issues
         from contact page and football
         federation personals will recieve
         them as email

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,11 +1,14 @@
 import express from "express"
 import * as newsController from "../controller/newsController.js"
 
+
 const router = express.Router()
 
 router.route("/").post(newsController.createNews);
 router.route("/").get(newsController.getAllNews);
 
 router.route("/:id").get(newsController.getANews);
+router.route("/:id").delete(newsController.deleteNews);
+router.route("/:id").put(newsController.updateNews);
 
 export default router
